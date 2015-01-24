3d9dddf0bab6361bff0ac337924d03002c26e112
Andross <blake.balick.schreiber@gmail.com>
2015-01-24 14:10:36 -0500

    Create map.js

---
+++ libraries/map.js
@@ -0,0 +1,59 @@
+
+var mapArray = [
+    [0, 0, 0, 0 ,0],
+    [0, 1, 0, 0 ,0],
+    [0, 0, 0, 0 ,0],
+    [0, 0, 0, 0 ,0],
+    [0, 0, 1, 1 ,0]
+];
+
+function drawMap(canvas) {
+    background = canvas;      
+    for (var i = 0; i < mapArray.length; i++) {
+        for (var j = 0; j < mapArray[i].length; j++) {
+            if (parseInt(mapArray[i][j]) == 0) {
+            var newTile = {
+    		// color: "#00A",
+    		tile: Tile("spaceship"),
+    		x: 28,
+    		y: 28,
+    		width:	60,
+    		height: 60,
+    		
+    		draw: function() {
+        	//canvas.fillStyle = this.color;
+        	// canvas.fillRect(this.x, this.y, this.width, this.height);
+        	this.tile.draw(canvas, this.x, this.y);
+	     }
+	   };
+
+		newTile.draw();
+               //console.log(newTile.x);
+            //  <!--canvas.drawImage(image1, x*28, y*28);-->
+	    }
+            if (parseInt(mapArray[i][j]) == 1) {
+	        var newTile = {
+    		// color: "#00A",
+    		tile: Tile("enemy"),
+    		x: j*28,
+    		y: i*28,
+    		width: 60,
+    		height: 60,
+    
+    		draw: function() {
+        	//canvas.fillStyle = this.color;
+        	// canvas.fillRect(this.x, this.y, this.width, this.height);
+        	this.tile.draw(canvas, this.x, this.y);
+    }
+};
+		newTile.draw();
+            //canvas.drawImage(image2, x*28, y*28);-->
+            }
+           
+            
+           //background.addChild(tile);-->
+        }
+    }
+   //canvas.addChild(background);-->     
+}
+
